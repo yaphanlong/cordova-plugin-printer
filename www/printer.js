420b51dcf6197f6716de19518bc9f259ad580cfa
Yap Han Long <hanlong@qiren.com.sg>
2022-02-09 15:04:51 +0800

    no message

--- www/printer.js
+++ www/printer.js
@@ -160,14 +160,14 @@ exports.pick = function (options, callback, scope)
     var fn     = this._createCallbackFn(callback, scope),
         params = this._mergeWithDefaults({ ui: options });
 
-    if (isIOS)
-    {
-        exec(fn, null, 'Printer', 'pick', [params]);
-    }
-    else if (fn)
-    {
-        fn(null);
-    }
+    // if (isIOS)
+    // {
+         exec(fn, null, 'Printer', 'pick', [params]);
+    // }
+    // else if (fn)
+    // {
+    //     fn(null);
+    // }
 };
 
 /**
